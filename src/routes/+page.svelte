df851dc3ce02afc2d1347d0f2baa1a2e8bd382d5
Rupsnigdha <83987752+Rupsnigdha@users.noreply.github.com>
2024-01-02 17:05:52 +0530

    Initial commit and google oauth

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    
+</script>
+
+<section>
+    <a href="/login">
+        <button type="button" class="btn variant-filled-secondary rounded-full px-12">login</button>
+    </a>
+</section>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let form;
+    import google from '$lib/assets/icons/google.png'
+</script>
+
+<section class="h-screen variant-filled-surface py-16 flex flex-col justify-end items-center">
+    <form method="post" action="?/OAuth2">
+        <button href="/" class="btn variant-filled-primary rounded-full px-16" type="submit">
+            <span><img src={google} alt=""></span>
+            <span>Get started</span>
+        </button>
+    </form>
+</section>
